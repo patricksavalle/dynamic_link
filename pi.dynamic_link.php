83c2b15830f41e2fd860ce5c860c27aabb35fa7b
Patrick Savalle <patrick@patricksavalle.com>
2020-08-05 10:03:56 +0200

    Now return {link_domain} too.

--- pi.dynamic_link.php
+++ pi.dynamic_link.php
@@ -22,66 +22,68 @@ class Dynamic_link {
 		if (strcasecmp($encoded, 'base64') === 0) {
 			$url = base64_decode($url);
 		}
-		$rmetas = ee()->cache->get('/dynamic_link/' . md5($url));
-		if (empty($rmetas))
+		$metadata = ee()->cache->get(__CLASS__ . '/' . md5($url));
+		if (empty($metadata))
 		{
 			$html = @file_get_contents($url);
 			if (empty($html)) {
-				$rmetas = ["crawled" => false];
+				$metadata = ["crawled" => false];
 			} else {
-				$rmetas = ["crawled" => true];
+				$metadata = ["crawled" => true];
 				libxml_use_internal_errors(true);
 				$doc = new DomDocument();
 				$doc->loadHTML($html);
 				$xpath = new DOMXPath($doc);
 
 				// first try og:
-				$rmetas['link_url'] = @$xpath->query('/*/head/meta[@property="og:url"]/@content')->item(0)->nodeValue;
-				$rmetas['link_title'] = @$xpath->query('/*/head/meta[@property="og:title"]/@content')->item(0)->nodeValue;;
-				$rmetas['link_description'] = @$xpath->query('/*/head/meta[@property="og:description"]/@content')->item(0)->nodeValue;;
-				$rmetas['link_image'] = @$xpath->query('/*/head/meta[@property="og:image"]/@content')->item(0)->nodeValue;;
+				$metadata['link_url'] = @$xpath->query('/*/head/meta[@property="og:url"]/@content')->item(0)->nodeValue;
+				$metadata['link_title'] = @$xpath->query('/*/head/meta[@property="og:title"]/@content')->item(0)->nodeValue;;
+				$metadata['link_description'] = @$xpath->query('/*/head/meta[@property="og:description"]/@content')->item(0)->nodeValue;;
+				$metadata['link_image'] = @$xpath->query('/*/head/meta[@property="og:image"]/@content')->item(0)->nodeValue;;
 
 				// than try twitter:
-				if (empty($rmetas['link_url'])) {
-					$rmetas['link_url'] = @$xpath->query('/*/head/meta[@name="twitter:url"]/@content')->item(0)->nodeValue;
+				if (empty($metadata['link_url'])) {
+					$metadata['link_url'] = @$xpath->query('/*/head/meta[@name="twitter:url"]/@content')->item(0)->nodeValue;
 				}
-				if (empty($rmetas['link_title'])) {
-					$rmetas['link_title'] = @$xpath->query('/*/head/meta[@name="twitter:title"]/@content')->item(0)->nodeValue;
+				if (empty($metadata['link_title'])) {
+					$metadata['link_title'] = @$xpath->query('/*/head/meta[@name="twitter:title"]/@content')->item(0)->nodeValue;
 				}
-				if (empty($rmetas['link_description'])) {
-					$rmetas['link_description'] = @$xpath->query('/*/head/meta[@name="twitter:description"]/@content')->item(0)->nodeValue;
+				if (empty($metadata['link_description'])) {
+					$metadata['link_description'] = @$xpath->query('/*/head/meta[@name="twitter:description"]/@content')->item(0)->nodeValue;
 				}
-				if (empty($rmetas['link_image'])) {
-					$rmetas['link_image'] = @$xpath->query('/*/head/meta[@name="twitter:image"]/@content')->item(0)->nodeValue;
+				if (empty($metadata['link_image'])) {
+					$metadata['link_image'] = @$xpath->query('/*/head/meta[@name="twitter:image"]/@content')->item(0)->nodeValue;
 				}
 
 				// than be opportunistic, try other tags
-				if (empty($rmetas['link_description'])) {
-					$rmetas['link_description'] = @$xpath->query('/*/head/meta[@name="description"]/@content')->item(0)->nodeValue;;
+				if (empty($metadata['link_description'])) {
+					$metadata['link_description'] = @$xpath->query('/*/head/meta[@name="description"]/@content')->item(0)->nodeValue;;
 				}
-				if (empty($rmetas['link_description'])) {
-					$rmetas['link_description'] = @$xpath->query('/*/head/meta[@name="keywords"]/@content')->item(0)->nodeValue;;
+				if (empty($metadata['link_description'])) {
+					$metadata['link_description'] = @$xpath->query('/*/head/meta[@name="keywords"]/@content')->item(0)->nodeValue;;
 				}
-				if (empty($rmetas['link_title'])) {
-					$rmetas['link_title'] = @$xpath->query('/*/head/title')->item(0)->nodeValue;
+				if (empty($metadata['link_title'])) {
+					$metadata['link_title'] = @$xpath->query('/*/head/title')->item(0)->nodeValue;
 				}
-				if (empty($rmetas['link_url'])) {
-					$rmetas['link_url'] = @$xpath->query('/*/head/link[@rel="canonical"]/@href')->item(0)->nodeValue;
+				if (empty($metadata['link_url'])) {
+					$metadata['link_url'] = @$xpath->query('/*/head/link[@rel="canonical"]/@href')->item(0)->nodeValue;
 				}
-				if (empty($rmetas['link_url'])) {
-					$rmetas['link_url'] = $url;
+				if (empty($metadata['link_url'])) {
+					$metadata['link_url'] = $url;
 				}
-				if (empty($rmetas['link_image'])) {
-					$rmetas['link_image'] = @$xpath->query('/*/head/link[@rel="apple-touch-icon"]/@href')->item(0)->nodeValue;
+				if (empty($metadata['link_image'])) {
+					$metadata['link_image'] = @$xpath->query('/*/head/link[@rel="apple-touch-icon"]/@href')->item(0)->nodeValue;
 				}
 			}
-			ee()->cache->save('/dynamic_link/' . md5($url), $rmetas, 60 * 60 * 24);
+			$metadata['link_domain'] = parse_url($metadata['link_url'], PHP_URL_HOST);
+			ee()->cache->save(__CLASS__ . '/' . md5($url), $metadata, 60 * 60 * 24);
 		}
-        $this->return_data = ee()->TMPL->parse_variables($tagdata, array($rmetas));
+        $this->return_data = ee()->TMPL->parse_variables($tagdata, array($metadata));
 	}
 
     function encode()
     {
         return base64_encode(ee()->TMPL->fetch_param('url'));
     }
+
 }
